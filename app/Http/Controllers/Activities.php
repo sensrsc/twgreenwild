9ec6223efc8fdd373e5db4c2d61a4ea7f161647c
Onion <wnrsfnw@gmail.com>
2018-03-04 15:53:11 +0800

    add new controller

---
+++ app/Http/Controllers/Activities.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Activities extends Controller
+{
+    //
+
+    public function index()
+    {
+    	echo 'in activities';
+    }
+}
