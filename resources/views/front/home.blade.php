b5b1109314d7856662d36d05f91522e87ae36506
MalumRubiginous <malum0929@hotmail.com>
2018-03-07 02:08:52 +0800

    fix: 調整首頁資料改以參數傳送

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -56,58 +56,10 @@
     ])
     @include('front.common.activities', [
         'section_title' => '季節推薦',
-        'activities' => array(
-            array(
-                'title' => '發現台灣之美 南澳鹿皮溪',
-                'type' => '朔溪',
-                'region' => '北部',
-                'cover' => asset('images/river.jpg'),
-                'price' => '1,097'
-            ),
-
-            array(
-                'title' => '發現台灣之美 南澳鹿皮溪',
-                'type' => '朔溪',
-                'region' => '北部',
-                'cover' => asset('images/river.jpg'),
-                'price' => '1,097'
-            ),
-
-            array(
-                'title' => '發現台灣之美 南澳鹿皮溪',
-                'type' => '朔溪',
-                'region' => '北部',
-                'cover' => asset('images/river.jpg'),
-                'price' => '1,097'
-            ),
-        )
+        'activities' => $seasonActivities
     ])
     @include('front.common.activities', [
         'section_title' => '熱門活動',
-        'activities' => array(
-            array(
-                'title' => '發現台灣之美 南澳鹿皮溪',
-                'type' => '朔溪',
-                'region' => '北部',
-                'cover' => asset('images/river.jpg'),
-                'price' => '1,097'
-            ),
-
-            array(
-                'title' => '發現台灣之美 南澳鹿皮溪',
-                'type' => '朔溪',
-                'region' => '北部',
-                'cover' => asset('images/river.jpg'),
-                'price' => '1,097'
-            ),
-
-            array(
-                'title' => '發現台灣之美 南澳鹿皮溪',
-                'type' => '朔溪',
-                'region' => '北部',
-                'cover' => asset('images/river.jpg'),
-                'price' => '1,097'
-            ),
-        )
+        'activities' => $hotActivities
     ])
 @endsection
