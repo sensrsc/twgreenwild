a7d999418719382851b2d72988d1ba7c5f51e787
Onion <wnrsfnw@gmail.com>
2018-03-07 14:44:12 +0800

    activities加上取得分類title, 活動array

--- app/Http/Controllers/Activities.php
+++ app/Http/Controllers/Activities.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\CategoryRepository;
+use App\Repositories\TourRepository;
 use Illuminate\Http\Request;
 
 class Activities extends Controller
 {
-    //
+    protected $tourRepository;
+
+    public function __construct(TourRepository $repository, CategoryRepository $categoryRepository)
+    {
+        $this->tourRepository     = $repository;
+        $this->categoryRepository = $categoryRepository;
+    }
 
     public function index(Request $request, $id)
     {
-    	
-    	return view('front.activities');
+        $category      = $this->categoryRepository->getById($id);
+        $categoryTitle = $category->c_title ?? '';
+        $activities    = $this->tourRepository->getByCategoryJoin($id);
+
+        return view('front.activities', compact('activities', 'categoryTitle'));
     }
 }
