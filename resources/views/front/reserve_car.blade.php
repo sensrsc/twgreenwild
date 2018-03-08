437ee538ccba43b2ab7d07c71ea80c23a4d2440f
julian74542 <julian7454@yahoo.com.tw>
2018-03-09 00:54:59 +0800

    add form style

--- resources/views/front/reserve_car.blade.php
+++ resources/views/front/reserve_car.blade.php
@@ -7,39 +7,6 @@
         .airport-block__back {
             display: none;
         }
-        .bd-example {
-            position: relative;
-            padding: 1rem;
-            border: solid #f7f7f9;
-            border-width: .2rem 0 0;
-        }
-        .bd-example p {
-            color: #818181;
-        }
-        .bd-example input, 
-        .bd-example select {
-            color: #818181;
-            display: inline-block;
-            padding: 0.375rem 0.75rem;
-            font-size: 1rem;
-            line-height: 1.5;
-            color: #495057;
-            background-color: #fff;
-            background-clip: padding-box;
-            border: 1px solid #ced4da;
-            border-radius: 0.25rem;
-            transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-        }
-        .bd-example select {
-            height: calc(2.25rem + 2px);
-        }
-        .bd-example option {
-            font-weight: normal;
-            display: block;
-            white-space: pre;
-            min-height: 1.2em;
-            padding: 0px 2px 1px;
-        }
     </style>
     <div class="bd-example">
         <span>類型</span>
