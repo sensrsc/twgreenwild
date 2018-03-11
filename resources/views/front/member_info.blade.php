3946a82ef89543baa05c5859df7828f3d96cc1eb
julian74542 <julian7454@yahoo.com.tw>
2018-03-11 18:59:02 +0800

    feat: member info form margin auto

--- resources/views/front/member_info.blade.php
+++ resources/views/front/member_info.blade.php
@@ -2,44 +2,51 @@
 @section('title', '會員資料頁')
 @section('content')
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<style>
+	.member-info span {
+		text-align: right;
+		line-height: 2.5;
+	}
+</style>
 <h1 class="title--mobile">會員資料</h1>
+<div class="container member-info" style="text-align: center;">
 <form class="bd-example" method="post">
 	{{ csrf_field() }}
 	@if ($user->fb_id)
 		<button class="btn-fb btn btn-primary">已使用 Fackbook 帳號登入</button>
 	@endif
 	<p>登入資料</p>
-	<p>
-        <span>帳號</span>
+	<p class="row">
+        <span class="col-4 col-md-5">帳號</span>
         <input disabled="disabled" type="mail" name="account" value="{{ $user->u_account }}">
     </p>
-    <!-- <p>
-        <span>密碼</span>
+    <!-- <p class="row">
+        <span class="col-4 col-md-5">密碼</span>
         <input type="password">
-        <span>
+        <span class="col-4 col-md-5">
         	<button class="btn-fb btn btn-danger">修改密碼</button>
         </span>	
  
     </p> -->
 	<p>登入資料</p>
-	<p>
-		<span>姓名</span>
+	<p class="row">
+		<span class="col-4 col-md-5">姓名</span>
 		<input type="text" name="name" value="{{ $user->u_name }}">
 	</p>
-	<p>
-		<span>身分證號碼</span>
+	<p class="row">
+		<span class="col-4 col-md-5">身分證號碼</span>
 		<input type="text" name="identity" value="{{ $user->u_identity }}">
 		<p>*非台灣客戶請輸入身份識別碼</p>
 	</p>
-	<p>
-		<span>性別</span>
+	<p class="row">
+		<span class="col-4 col-md-5">性別</span>
 		<select name="gender" id="">
 			<option value="1" {{ $user->u_gender == 1? 'selected' : '' }}>男</option>
 			<option value="0" {{ $user->u_gender == 0? 'selected' : '' }}>女</option>
 		</select>
 	</p>
-	<p>
-		<span>生日</span>
+	<p class="row">
+		<span class="col-4 col-md-5">生日</span>
 		<select name="year" id="">
 			<option value="" selected disabled hidden>年</option>
 			@for ($year = date('Y'); $year > (date('Y') -50); $year --)
@@ -60,40 +67,40 @@
 			@endfor
 		</select>
 	</p>
-	<p>
-		<span>身高</span>
+	<p class="row">
+		<span class="col-4 col-md-5">身高</span>
 		<input type="text" name="height" value="{{ $user->u_height }}">
-		<span>公分</span>
+		<span class="col-4 col-md-5">公分</span>
 	</p>
-	<p>
-		<span>體重</span>
+	<p class="row">
+		<span class="col-4 col-md-5">體重</span>
 		<input type="text" name="weight" value="{{ $user->u_weight }}">
-		<span>公斤</span>
+		<span class="col-4 col-md-5">公斤</span>
 	</p>
-	<p>
-		<span>腳掌</span>
+	<p class="row">
+		<span class="col-4 col-md-5">腳掌</span>
 		<input type="text" name="foot" value="{{ $user->u_foot }}">
-		<span>公分</span>
+		<span class="col-4 col-md-5">公分</span>
 	</p>
-	<p>
-		<span>電話</span>
+	<p class="row">
+		<span class="col-4 col-md-5">電話</span>
 		<input type="tel" name="phone" value="{{ $user->u_phone }}">
 	</p>
-	<p>
-		<span>地址</span>
+	<p class="row">
+		<span class="col-4 col-md-5">地址</span>
 		<input type="text" name="address" value="{{ $user->u_address }}">
 	</p>
-	<!-- <p>
-		<span>信箱</span>
+	<!-- <p class="row">
+		<span class="col-4 col-md-5">信箱</span>
 		<input type="mail">
 	</p> -->
 	<p>緊急聯絡人資料</p>
-	<p>
-		<span>姓名</span>
+	<p class="row">
+		<span class="col-4 col-md-5">姓名</span>
 		<input type="text" name="emergency_name" value="{{ $user->u_emergency_name }}">
 	</p>
-	<p>
-		<span>電話</span>
+	<p class="row">
+		<span class="col-4 col-md-5">電話</span>
 		<input type="tel" name="emergency_phone" value="{{ $user->u_emergency_phone }}">
 	</p>
 	<button type="submit" class="btn-fb btn btn-info">儲存修改</button>
@@ -104,5 +111,5 @@
 		{!! $msg !!}
 	</div>
 @endif
-
+</div>
 @endsection
