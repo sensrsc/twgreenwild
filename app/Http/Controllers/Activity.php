8230e7e26d0581550abfd0f78801837b07100fec
Onion <wnrsfnw@gmail.com>
2018-03-12 22:40:44 +0800

    view變數activityDate, tour

--- app/Http/Controllers/Activity.php
+++ app/Http/Controllers/Activity.php
@@ -2,15 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\TourRepository;
 use Illuminate\Http\Request;
 
 class Activity extends Controller
 {
-    //
-
-    public function index()
+    public function __construct(TourRepository $repository)
     {
-    	return view('front.activity');
+        $this->tourRepository     = $repository;
+    }
+
+    public function index(Request $request, $id)
+    {
+    	$data = $this->tourRepository->getByID($id);
+
+    	$tour = $data? $data->toArray() : [];
+        $days = $data->days_apply ?? 0;
+    	$activityDate = [
+    		'apply_date' => date('Y-m-d', strtotime('+' . $days . 'days', time())),
+    		'ready_date' => [],	
+    	];
+
+    	return view('front.activity', compact('activityDate', 'tour'));
     }
 
 }
