1fe7489ea8d4ee031eaf5be511427de726347a80
MalumRubiginous <malum0929@hotmail.com>
2018-03-13 23:11:14 +0800

    feat: 新增活動訂購的表單基本架構

--- resources/views/front/activity.blade.php
+++ resources/views/front/activity.blade.php
@@ -2,8 +2,87 @@
 @section('title', '分類列表')
 @section('content')
     @include('front.common.carousel')
-    <div>calender＋按鈕</div>
+
+    <style>
+        #calendar .full a {
+            background-color: #ff5722 !important;
+            background-image :none !important;
+            color: #ffffff !important;
+        }
+    </style>
+    <div class="activity-form">
+        <div id="calendar"></div>
+        <div id="form-container">
+            <ul class="nav nav-tabs" id="form-tab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="single-tab" data-toggle="tab" href="#single" role="tab" aria-controls="single" aria-selected="true">個人報名</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="group-tab" data-toggle="tab" href="#group" role="tab" aria-controls="group" aria-selected="false">團體報名</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="form-tab-content">
+                <div class="tab-pane fade show active" id="single" role="tabpanel" aria-labelledby="single-tab">
+                    <div class="single-form">@include('front.common.single_apply_form')</div>
+                </div>
+                <div class="tab-pane fade" id="group" role="tabpanel" aria-labelledby="group-tab">
+                    <div class="group-form">@include('front.common.group_apply_form')</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr>
     <div>上稿區</div>
-    <div>表單 popup</div>
+
+    <script>
+        $(document).ready(function() {
+            var singleTab = document.getElementById('single-tab');
+            var groupTab = document.getElementById('group-tab');
+            var singleContent = document.getElementById('single');
+            var groupContent = document.getElementById('group');
+            var singleApplyDate = document.getElementById('single-apply-date');
+            var groupApplyDate = document.getElementById('group-apply-date');
+            var activityDate = JSON.parse('{!! json_encode($activityDate) !!}');
+
+            var eventDates = {};
+            activityDate.ready_date.forEach(function (date) {
+                eventDates[new Date(date)] = new Date(date);
+            });
+
+            singleApplyDate.innerText = activityDate.apply_date;
+            groupApplyDate.innerText = activityDate.apply_date;
+
+            // datepicker
+            $('#calendar').datepicker({
+                dateFormat: 'yy-mm-dd',
+                minDate: new Date(activityDate.apply_date),
+                beforeShowDay: function (date) {
+                    var highlight = eventDates[date];
+                    if(highlight) {
+                         return [true, "full", highlight];
+                    } else {
+                         return [true, '', ''];
+                    }
+                },
+                onSelect: function (dateText) {
+                    singleApplyDate.innerText = dateText;
+                    groupApplyDate.innerText = dateText;
+                },
+            });
+
+            singleTab.addEventListener('click', function (e) {
+                e.preventDefault();
+                singleContent.classList.add('show');
+                groupContent.classList.remove('show');
+            });
+
+            groupTab.addEventListener('click', function (e) {
+                e.preventDefault();
+                singleContent.classList.remove('show');
+                groupContent.classList.add('show');
+            });
+        });
+    </script>
 @endsection
 
---
+++ resources/views/front/common/single_apply_form.blade.php
@@ -0,0 +1,144 @@
+<form>
+    <div class="form-group row">
+        <label class="col-4 col-form-label">行程名稱</label>
+        <div class="col-8">{{$tour['t_title']}}</div>
+    </div>
+    <div class="form-group row">
+        <label class="col-4 col-form-label">行程編號</label>
+        <div class="col-8">{{$tour['t_id']}}</div>
+    </div>
+    <div class="form-group row">
+        <label class="col-4 col-form-label">訂購日期</label>
+        <div class="col-8" id="single-apply-date"></div>
+    </div>
+    <div class="form-group row">
+        <label class="col-4 col-form-label">參加人員為</label>
+        <div class="col-8">
+            <div class="form-check form-check-inline">
+                <label for="single-type-adult" class="form-check-label">
+                    <input class="form-check-input" type="radio" name="type" id="single-type-adult" value="">大人
+                </label>
+            </div>
+            <div class="form-check form-check-inline">
+                <label for="single-type-child" class="form-check-label">
+                    <input class="form-check-input" type="radio" name="type" id="single-type-child" value="">小孩
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-4 col-form-label">是否需要車輛接送</label>
+        <div class="col-8">
+            <div class="form-check form-check-inline">
+                <label for="single-transfer-yes" class="form-check-label">
+                    <input class="form-check-input" type="radio" name="transfer" id="single-transfer-yes" value="">是
+                </label>
+            </div>
+            <div class="form-check form-check-inline">
+                <label for="single-transfer-no" class="form-check-label">
+                    <input checked class="form-check-input" type="radio" name="transfer" id="single-transfer-no" value="">否
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-name" class="col-4 col-form-label">姓名</label>
+        <div class="col-8">
+            <input class="form-control" type="text" value="" id="single-name">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-4 col-form-label">性別</label>
+        <div class="col-8">
+            <div class="form-check form-check-inline">
+                <label for="single-gender_M" class="form-check-label">
+                    <input class="form-check-input" type="radio" name="gender" id="single-gender_M" value="gender_M">男
+                </label>
+            </div>
+            <div class="form-check form-check-inline">
+                <label for="single-gender_F" class="form-check-label">
+                    <input class="form-check-input" type="radio" name="gender" id="single-gender_F" value="gender_F">女
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-tel" class="col-4 col-form-label">電話</label>
+        <div class="col-8">
+            <input class="form-control" type="tel" value="" id="single-tel">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-address" class="col-4 col-form-label">地址</label>
+        <div class="col-8">
+            <input class="form-control" type="text" value="" id="single-address">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-email" class="col-4 col-form-label">Email</label>
+        <div class="col-8">
+            <input class="form-control" type="email" value="" id="single-email">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-id" class="col-4 col-form-label">身份證號</label>
+        <div class="col-8">
+            <input class="form-control" type="text" value="" id="single-id">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-birth" class="col-4 col-form-label">生日</label>
+        <div class="col-8">
+            <input class="form-control" type="date" value="" id="single-birth">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-height" class="col-4 col-form-label">身高</label>
+        <div class="col-8">
+            <input class="form-control" type="number" value="" id="single-height">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-width" class="col-4 col-form-label">體重</label>
+        <div class="col-8">
+            <input class="form-control" type="number" value="" id="single-width">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-contact-name" class="col-4 col-form-label">緊急聯絡人姓名</label>
+        <div class="col-8">
+            <input class="form-control" type="text" value="" id="single-contact-name">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-contact-tel" class="col-4 col-form-label">緊急聯絡人電話</label>
+        <div class="col-8">
+            <input class="form-control" type="tel" value="" id="single-contact-tel">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-4 col-form-label">金額</label>
+        <div class="col-8">{{$tour['t_price']}}</div>
+    </div>
+    <div class="form-group row">
+        <label class="col-4 col-form-label">付款方式</label>
+        <div class="col-8">
+            <div class="form-check form-check-inline">
+                <label for="single-pay-credit" class="form-check-label">
+                    <input class="form-check-input" type="radio" name="pay" id="single-pay-credit" value="">信用卡
+                </label>
+            </div>
+            <div class="form-check form-check-inline">
+                <label for="single-pay-atm" class="form-check-label">
+                    <input class="form-check-input" type="radio" name="pay" id="single-pay-atm" value="">ATM
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="single-note" class="col-4 col-form-label">備註</label>
+        <div class="col-8">
+            <input class="form-control" type="text" value="" id="single-note">
+        </div>
+    </div>
+</form>
