ea948b55da52f9aa1ebba4bbdc9ba37270269617
MalumRubiginous <malum0929@hotmail.com>
2018-03-13 15:59:03 +0800

    fix: adjust links

--- resources/views/front/activities.blade.php
+++ resources/views/front/activities.blade.php
@@ -5,6 +5,7 @@
         'section_title' => false,
         'activities' => array(
             array(
+                'id' => '1',
                 'title' => '發現台灣之美 南澳鹿皮溪',
                 'type' => '朔溪',
                 'region' => '北部',
@@ -13,6 +14,7 @@
             ),
 
             array(
+                'id' => '1',
                 'title' => '發現台灣之美 南澳鹿皮溪',
                 'type' => '朔溪',
                 'region' => '北部',
@@ -21,6 +23,7 @@
             ),
 
             array(
+                'id' => '1',
                 'title' => '發現台灣之美 南澳鹿皮溪',
                 'type' => '朔溪',
                 'region' => '北部',
@@ -29,6 +32,7 @@
             ),
 
             array(
+                'id' => '1',
                 'title' => '發現台灣之美 南澳鹿皮溪',
                 'type' => '朔溪',
                 'region' => '北部',
@@ -37,6 +41,7 @@
             ),
 
             array(
+                'id' => '1',
                 'title' => '發現台灣之美 南澳鹿皮溪',
                 'type' => '朔溪',
                 'region' => '北部',
@@ -45,6 +50,7 @@
             ),
 
             array(
+                'id' => '1',
                 'title' => '發現台灣之美 南澳鹿皮溪',
                 'type' => '朔溪',
                 'region' => '北部',
