2150e73da6ab598c80618e54840d4185f82cee9d
MalumRubiginous <malum0929@hotmail.com>
2018-03-14 09:03:30 +0800

    新增輪播圖片功能

--- resources/views/front/common/carousel.blade.php
+++ resources/views/front/common/carousel.blade.php
@@ -1,1 +1,24 @@
-<section class="carousel"></section>
+<section class="carousel">
+<div id="activity-carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <img class="d-block img-fluid" src="http://fakeimg.pl/1200x400/?text=picture_1" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block img-fluid" src="http://fakeimg.pl/1200x400/?text=picture_2" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block img-fluid" src="http://fakeimg.pl/1200x400/?text=picture_3" alt="Third slide">
+        </div>
+    </div>
+
+    <a class="carousel-control-prev" href="#activity-carousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#activity-carousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+</section>
